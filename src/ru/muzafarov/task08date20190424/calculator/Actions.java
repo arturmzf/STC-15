5f61d70adf893e9e6a8a0091b8bc8ad6522e8971
Artur Muzafarov <art.mzf@gmail.com>
2019-06-01 01:16:48 +0300

    The Task 08 (24.04.2019)

---
+++ src/ru/muzafarov/task08date20190424/calculator/Actions.java
@@ -0,0 +1,83 @@
+package ru.muzafarov.task08date20190424.calculator;
+
+public class Actions {
+
+    // Сложение
+    public static int addition(int a1, int a2) {
+        return a1 + a2;
+    }
+
+    public static double addition(double a1, int a2) {
+        return a1 + a2;
+    }
+
+    public static double addition(int a1, double a2) {
+        return a1 + a2;
+    }
+
+    public static double addition(double a1, double a2) {
+        return a1 + a2;
+    }
+
+    // Вычитание
+    public static int subtraction(int s1, int s2) {
+        return s1 - s2;
+    }
+
+    public static double subtraction(double s1, int s2) {
+        return s1 - s2;
+    }
+
+    public static double subtraction(int s1, double s2) {
+        return s1 - s2;
+    }
+
+    public static double subtraction(double s1, double s2) {
+        return s1 - s2;
+    }
+
+    // Умножение
+    public static int multiplication(int m1, int m2) {
+        return m1 * m2;
+    }
+
+    public static double multiplication(double m1, int m2) {
+        return m1 * m2;
+    }
+
+    public static double multiplication(int m1, double m2) {
+        return m1 * m2;
+    }
+
+    public static double multiplication(double m1, double m2) {
+        return m1 * m2;
+    }
+
+    // Деление
+    public static double division(int d1, int d2) {
+        return d1 / d2;
+    }
+
+    public static double division(double d1, int d2) {
+        return d1 / d2;
+    }
+
+    public static double division(int d1, double d2) {
+        return d1 / d2;
+    }
+
+    public static double division(double d1, double d2) {
+        return d1 / d2;
+    }
+
+
+    // Деление
+    public static double percent(int p1, int p2) {
+        return p1 / 100 * p2;
+    }
+
+    public static double percent(double p1, int p2) {
+        return p1 / 100 * p2;
+    }
+
+}
---
+++ src/ru/muzafarov/task08date20190424/calculator/Calculator.java
@@ -0,0 +1,62 @@
+package ru.muzafarov.task08date20190424.calculator;
+
+import java.util.Scanner;
+//import ru.muzafarov.task08date20190424.calculator.Actions;
+//import static ru.muzafarov.task08date20190424.calculator.Actions.addition;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+
+        Scanner scanner1 = new Scanner(System.in);
+        System.out.println("Пожалуйста, введите первый операнд и нажмите ENTER:");
+        String operand1 = scanner1.nextLine();
+
+        Scanner scannerOperator = new Scanner(System.in);
+        System.out.println("Пожалуйста, введите номер действия, которое Вы хотите совершить:");
+        System.out.println("1 - сложение;");
+        System.out.println("2 - вычитание;");
+        System.out.println("3 - умножение;");
+        System.out.println("4 - деление;");
+        System.out.println("5 - взятие процента от числа.");
+        int operator = scannerOperator.nextInt();
+
+        switch (operator) {
+            case 1:
+                Scanner scanner2 = new Scanner(System.in);
+                System.out.println("Пожалуйста, введите второй операнд и нажмите ENTER:");
+                String operand2 = scanner2.nextLine();
+                System.out.println("Результат операции: " + Actions.addition(operand1, operand2));
+                break;
+            case 2:
+                Scanner scanner3 = new Scanner(System.in);
+                System.out.println("Пожалуйста, введите второй операнд и нажмите ENTER:");
+                String operand3 = scanner3.nextLine();
+                System.out.println("Результат операции: " + Actions.subtraction(operand1, operand3));
+                break;
+            case 3:
+                Scanner scanner4 = new Scanner(System.in);
+                System.out.println("Пожалуйста, введите второй операнд и нажмите ENTER:");
+                String operand4 = scanner4.nextLine();
+                System.out.println("Результат операции: " + Actions.multiplication(operand1, operand4));
+                break;
+            case 4:
+                Scanner scanner5 = new Scanner(System.in);
+                System.out.println("Пожалуйста, введите второй операнд и нажмите ENTER:");
+                String operand5 = scanner5.nextLine();
+                System.out.println("Результат операции: " + Actions.division(operand1, operand5));
+                break;
+            case 5:
+                Scanner scanner6 = new Scanner(System.in);
+                System.out.println("Пожалуйста, введите количество процентов и нажмите ENTER:");
+                int operand6 = scanner6.nextInt();
+                System.out.println("Результат операции: " + Actions.percent(operand1, operand6));
+                break;
+            default:
+                System.out.println("Вы что-то не то ввели, увы...");
+                break;
+        }
+
+    }
+
+}
