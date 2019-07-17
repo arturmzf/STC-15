470ae1e36a59e8ba0a98590a251598c4848bb29b
Artur Muzafarov <art.mzf@gmail.com>
2019-07-17 20:32:16 +0300

    The Task 18 (24.05.2019)

---
+++ src/ru/muzafarov/task18date20190524/Task18App.java
@@ -0,0 +1,19 @@
+package ru.muzafarov.task18date20190524;
+
+import java.io.*;
+import java.util.*;
+
+public class Task18App {
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+
+        ObjectInputStream in = new ObjectInputStream(
+                new FileInputStream("src/ru/muzafarov/task18date20190524/files/windows-1251.txt"));
+
+        /*
+        File -> String
+         */
+
+    }
+
+}
