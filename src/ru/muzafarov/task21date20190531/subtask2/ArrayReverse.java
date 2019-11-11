69565b4c6bb8b2c727ae03805d7bdcad54bb3c4b
Artur Muzafarov <art.mzf@gmail.com>
2019-11-12 01:57:15 +0300

    The Task 21 (31.05.2019) (edit 02)

--- src/ru/muzafarov/task21date20190531/subtask2/ArrayReverse.java
+++ src/ru/muzafarov/task21date20190531/subtask2/ArrayReverse.java
@@ -3,9 +3,15 @@ package ru.muzafarov.task21date20190531.subtask2;
 public class ArrayReverse {
 
     private int[][] array;
+    boolean[] flags;
 
     public ArrayReverse(int[][] array) {
         this.array = array;
+        // НЕ ОЧЕНЬ!!!
+        flags = new boolean[array[0].length];
+        for (int i=0; i < flags.length; i++) {
+            flags[i] = true;
+        }
     }
 
     public int[][] getArray() {
@@ -40,10 +46,20 @@ public class ArrayReverse {
 
             for (int j = 0; j < array[i].length; j++) {
 
-                int tmp = array[i][j];
-                array[i][j] = array[i][array[i].length - (j + 1)];
-                array[i][array[i].length - (j + 1)] = tmp;
+                if (this.flags[j]) {
 
+                    int tmp = array[i][j];
+                    array[i][j] = array[i][array[i].length - (j + 1)];
+                    array[i][array[i].length - (j + 1)] = tmp;
+
+                    this.flags[this.flags.length - (j + 1)] = false;
+
+                }
+
+            }
+
+            for (int k=0; k<this.flags.length; k++) {
+                this.flags[k] = true;
             }
 
         }
@@ -52,7 +68,7 @@ public class ArrayReverse {
 
     public void showResult() {
 
-        System.out.println("\n\nИзменённый массивчик:");
+        System.out.println("Изменённый массивчик:");
 
         for (int i = 0; i < array.length; i++) {
 
